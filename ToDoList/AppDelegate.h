2aa2a255c59840a9d76e6c579409ec17b78e6d49
Attila Csala <attilacsala@gmail.com>
2013-10-28 13:21:12 +0100

    Initial Commit

---
+++ ToDoList/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by Attila Csala on 10/28/13.
+//  Copyright (c) 2013 Attila Csala. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
